3382d1ac6d2ce468463561e3e5b34ba15d2849a2
AbanoubEmadM <83979832+AbanoubEmadM@users.noreply.github.com>
2022-07-11 16:51:16 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,6 +5,8 @@ let submitBtn = document.getElementById("submit-btn");
 submitBtn.onclick = function(){
     if(email.value !== "" && password.value !== ""){
         window.location = 'index.html'
+        localStorage.setItem("user_name",`${email.value} ${password.value}`)
+
     }else{
         swal("Sorry", "You Have To Write All Info", "error");
     }
