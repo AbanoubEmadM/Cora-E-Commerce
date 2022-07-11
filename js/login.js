647b3cefd8f4aeed88d3e79bac838cbd2754df14
AbanoubEmadM <83979832+AbanoubEmadM@users.noreply.github.com>
2022-07-11 17:53:08 +0200

    c

--- js/login.js
+++ js/login.js
@@ -6,6 +6,7 @@ submitBtn.onclick = function(){
     if(email.value !== "" && password.value !== ""){
         window.location = 'index.html'
         localStorage.setItem("user_name",`${email.value} ${password.value}`)
+    }else{
         swal("Sorry", "You Have To Write All Info", "error");
     }
 }
